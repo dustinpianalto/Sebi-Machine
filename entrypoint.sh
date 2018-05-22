626379488d757bd306b144943be1ab1c0036fcd8
PuffDip <PuffDip@m.com>
2018-05-22 15:41:21 +0200

    lazy cheeze :^)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python3.6 run.py > bot-log.txt 2>&1 &
-nodejs foobar.js > node-log.txt 2>&1 &
+nodejs run.js > node-log.txt 2>&1 &
 
 for job in $(jobs -p); do
     echo Waiting for ${job} to terminate.
