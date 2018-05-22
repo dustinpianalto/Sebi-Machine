5261a520028e2b1b9ffd6ff50e00aed20b05e907
annihilator708 <annihilator708@hotmail.com>
2018-05-23 00:16:56 +0200

    Add kick command

--- src/cogs/contributors.py
+++ src/cogs/contributors.py
@@ -2,7 +2,7 @@
 # -*- coding: <encoding name> -*-
 
 from discord.ext import commands
-import os
+import discord
 import traceback
 
 class Upload:
@@ -82,11 +82,28 @@ class Upload:
             await ctx.send(f'Loaded `{extension}`.')
 
     @commands.command()
-    async def err(self, ctx):
-        """triggers error to test traceback"""
-        await ctx.send(a)
+    async def kick(self, ctx, member: discord.Member = None):
+        """
+        Kick a discord member from your server.
+        Only contributors can use this command
 
+        Usage:
+          - kick <discord.member>
 
+        """
+        await ctx.trigger_typing()
+        if ctx.author.id not in self.bot.ownerlist:
+            return await ctx.send('Only my contributors can use me like this :blush:', delete_after=10)
+
+        if member is None:
+            await ctx.send('Are you sure you are capable of this command?')
+        try:
+            await member.kick()
+            await ctx.send(f'You kicked **`{member.name}`** from **`{ctx.guild.name}`**')
+
+        except Exception as e:
+            await ctx.send('You may not use this command you do not have permission in server:\n\n**`{ctx.guild.name}`**'
+                           f'\n\n```py\n{e}\n```')
 
 def setup(bot):
     bot.add_cog(Upload(bot))
