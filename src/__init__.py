4ce771d0eaaa0653d451bac8481d155d6d8ad117
Espy | Neko | 404 <34942042+neko404notfound@users.noreply.github.com>
2018-05-22 22:22:06 +0100

    Init files (#10)
    * Create __init__.py
    
    * Create __init__.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Create __init__.py
    
    * Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+"""
+Sebi-Machine.
+"""
+
+__author__ = 'Annihilator708'
+# TODO: add yourselves here. I can't remember everyones handles.
+__contributors__ = (__author__, 'Neko404NotFound',)
+__license__ = 'MIT'
+__title__ = 'Sebi-Machine'
+__version__ = 'tbd'
+
+__repository__ = f'https://github.com/{__author__}/{__title__}'
+__url__ = __repository__
