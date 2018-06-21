001d6aa0ac0eeb90b474289c339569c6e74d3bf6
neko404notfound <nekozilla404notfound@gmail.com>
2018-06-21 09:31:02 +0100

    Removed execution permission.

--- src/__init__.py
+++ src/__init__.py
@@ -5,11 +5,11 @@ Sebi-Machine.
 """
 
 __author__ = "Annihilator708"
-# TODO: add yourselves here. I can't remember everyones handles.
 __contributors__ = (__author__, "Neko404NotFound", "Dusty.P", "davfsa", "YashKandalkar")
+
 __license__ = "MIT"
 __title__ = "Sebi-Machine"
-__version__ = "tbd"
+__version__ = "0.0.1"
 
 __repository__ = f"https://github.com/{__author__}/{__title__}"
 __url__ = __repository__
